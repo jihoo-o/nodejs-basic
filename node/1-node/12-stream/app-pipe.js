94a051b45b3e12041e9cb63965265254a7a0c74d
kmseonhwa <seonhwakei@gmail.com>
2022-02-08 19:30:29 +0900

    Add: pipe

--- node/1-node/12-stream/app-pipe.js
+++ node/1-node/12-stream/app-pipe.js
@@ -1,3 +1,10 @@
 const fs = require('fs');
+const zlib = require('zlib');
 
-const readStream = fs.createReadStream('./file.txt', {});
+const readStream = fs.createReadStream('./file.txt');
+const zlibStream = zlib.createGzip(); // 압축 스트림도 연결
+const writeStream = fs.createWriteStream('./file4.zip');
+const piping = readStream.pipe(zlibStream).pipe(writeStream);
+piping.on('finish', () => {
+    console.log('done!');
+});
