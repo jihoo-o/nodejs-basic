d01856cdadc7897d09d82ea87ee181ea08065a52
kmseonhwa <seonhwakei@gmail.com>
2022-02-10 17:20:12 +0900

    Add: html Response

--- node/5-node-server/1-http.js
+++ node/5-node-server/1-http.js
@@ -1,16 +1,20 @@
 const http = require('http');
+const fs = require('fs');
 
 // console.log(http.STATUS_CODES);
 // console.log(http.METHODS);
 
 const server = http.createServer((req, res) => {
-    console.log('incoming...');
-    console.log(req.headers);
-    console.log(req.httpVersion);
-    console.log(req.method);
-    console.log(req.url);
-    res.write('welcome!');
-    res.end();
+    const url = req.url;
+    res.setHeader('Content-Type', 'text/html');
+    if (url === '/') {
+        fs.createReadStream('./html/index.html').pipe(res);
+    } else if (url === '/courses') {
+        fs.createReadStream('./html/courses.html').pipe(res);
+    } else {
+        fs.createReadStream('./html/not-found.html').pipe(res);
+    }
+    // res.end();
 });
 
 server.listen(8080); // port
