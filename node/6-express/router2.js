dd67c6629ece51f66671fd86907b4bbae0c203d5
kmseonhwa <seonhwakei@gmail.com>
2022-02-11 18:36:54 +0900

    Add: express - internal middleware

--- node/6-express/router2.js
+++ node/6-express/router2.js
@@ -2,9 +2,22 @@ import express from 'express';
 import postRouter from './router/post.js';
 import userRouter from './router/user.js';
 
-const app = express();
+const app = express(); //
 
-app.use(express.json());
+const options = {
+    dotfiles: 'ignore',
+    etag: false,
+    index: false,
+    maxAge: '1d',
+    redirect: false,
+    setHeaders: function (res, path, stat) {
+        res.set('x-timestamp', Date.now());
+    },
+};
+
+app.use(express.json()); // REST API -> Body
+app.use(express.urlencoded({ extended: false })); // HTML Form -> Body
+app.use(express.static('public', options)); // 리소스 접근 허용
 
 // ✨
 app.use('/posts', postRouter);
