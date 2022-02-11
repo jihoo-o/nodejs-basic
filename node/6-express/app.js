c2b6af748442d4cb0b5393ba03b9dcdd1de4e584
kmseonhwa <seonhwakei@gmail.com>
2022-02-11 16:37:58 +0900

    Add: express - request

--- node/6-express/app.js
+++ node/6-express/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
 const app = express();
 
-app.get('/', (req, res, next) => {
-    console.log('get');
+app.get('/sky/:id', (req, res, next) => {
+    console.log(req.path);
+    console.log(req.headers);
+    console.log(req.params);
+    console.log(req.query);
     res.send('hi');
 });
 
