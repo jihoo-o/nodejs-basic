5e7e1099f22150fb06ee470172d7972dbd72dbed
kmseonhwa <seonhwakei@gmail.com>
2022-02-11 17:13:45 +0900

    Add: express - POST 처리

--- node/6-express/app.js
+++ node/6-express/app.js
@@ -1,45 +1,10 @@
 import express from 'express';
 const app = express();
 
-// app.all 🆚 app.use
-app.all('/api', (req, res, next) => {
-    console.log('all');
-    next();
-});
+app.use(express.json());
 
-app.use('/sky', (req, res, next) => {
-    console.log('use');
-    next();
-});
-
-app.get(
-    '/',
-    (req, res, next) => {
-        console.log('first');
-        // res.send('hihihi');
-        next('route');
-    },
-    (req, res, next) => {
-        console.log('second');
-        next();
-    }
-);
-
-app.get('/', (req, res, next) => {
-    console.log('third');
-    // next(new Error('error occured!'));
-    next();
-});
-
-// 유효하지 않은 URL
-app.use((req, res, next) => {
-    res.status(404).send('Not Available!');
-});
-
-// 마지막 미들웨어에서 에러처리
-app.use((error, req, res, next) => {
-    console.error(error);
-    res.status(500).send('Sorry, try later!');
+app.post('/', (req, res, next) => {
+    console.log(req.body);
 });
 
 app.listen(8080);
