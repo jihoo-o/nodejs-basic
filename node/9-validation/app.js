19c7bd03abef156e18f27bc5c40bc6ac8112a44a
kmseonhwa <seonhwakei@gmail.com>
2022-02-14 11:31:39 +0900

    Add: validator

---
+++ node/9-validation/app.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import { body, validationResult } from 'express-validator';
+
+const app = express();
+app.use(express.json());
+
+/**
+ * body
+    {
+        "name": "S1",
+        "age": 1,
+        "job": {
+            "name": "DC Academy",
+            "title": "Student"
+        },
+        "email": "seonhwa@server.com"
+    }
+ */
+
+app.post(
+    '/users',
+    [
+        body('name')
+            .notEmpty()
+            .withMessage('이름을 입력해주세요')
+            .isLength({ min: 2 })
+            .withMessage('이름은 두 글자 이상이어야 합니다'),
+        body('age').notEmpty().isInt().withMessage('나이는 숫자여야 합니다'),
+        body('email').isEmail().withMessage('이메일 형식에 어긋납니다'),
+        body('job.name').notEmpty(),
+    ],
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ message: errors.array() });
+        }
+        console.log(req.body);
+        res.sendStatus(201);
+    }
+);
+
+app.get('/:email', param('email').notEmpty(), (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ message: errors.array() });
+    }
+    res.send('💌');
+});
+
+app.listen(8080);
