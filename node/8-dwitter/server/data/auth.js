61ae37d55ea70eae1968d7b3cf20561872cbb54f
kmseonhwa <seonhwakei@gmail.com>
2022-02-18 14:59:48 +0900

    Add: Dwitter - Sequelize와 auth data 연결

--- node/8-dwitter/server/data/auth.js
+++ node/8-dwitter/server/data/auth.js
@@ -1,24 +1,50 @@
-import { db } from '../db/database.js';
+import SQ from 'sequelize';
+import { db, sequelize } from '../db/database.js';
+
+// MODEL
+const DataTypes = SQ.DataTypes;
+const User = sequelize.define(
+    'user',
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true,
+        },
+        username: {
+            type: DataTypes.STRING(45),
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING(128),
+            allowNull: false,
+        },
+        name: {
+            type: DataTypes.STRING(128),
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING(128),
+            allowNull: false,
+        },
+        url: DataTypes.TEXT,
+    },
+    { timestamps: false }
+);
 
 export async function findByUsername(username) {
-    return db
-        .execute('SELECT * FROM users WHERE username=?', [username]) //
-        .then((result) => result[0][0]);
+    return User.findOne({ where: { username } }) //
+        .then((data) => data.dataValues);
 }
 
 export async function findById(id) {
-    return db
-        .execute('SELECT * FROM users WHERE id=?', [id]) //
-        .then((result) => result[0][0]);
+    console.log(id);
+    return User.findByPk(id) //
+        .then(console.log);
 }
 
 export async function createUser(user) {
-    // created.id -> insertId
-    const { username, password, name, email, url } = user;
-    return db
-        .execute(
-            'INSERT INTO users (username, password, name, email, url) VALUES (?,?,?,?,?)',
-            [username, password, name, email, url]
-        )
-        .then((result) => result[0].insertId);
+    return User.create(user) //
+        .then((data) => data.dataValues.id);
 }
