283a84f4fac5ef5d0d5c09996ae7e0ed0aebece3
kmseonhwa <seonhwakei@gmail.com>
2022-02-19 12:28:05 +0900

    Add: Dwitter - MongoDB와 auth data 연결

--- node/8-dwitter/server/data/auth.js
+++ node/8-dwitter/server/data/auth.js
@@ -1,34 +1,27 @@
-let users = [
-    {
-        id: '1',
-        username: 'bob',
-        // abcd1234
-        password:
-            '$2b$10$yrgFjMgBg4evcvUroI42M.WvlC34OedKmpmtVmKtXym6V5.F/pHa2',
-        name: 'Bob',
-        email: 'bob@gmail.com',
-    },
-    {
-        id: '2',
-        username: 'seonhwa',
-        // 12345
-        password:
-            '$2b$12$/oXrjLvglzFCcgaN1pm/AOtSugsREXWkCPCktnyFhg8ThkSpLsFuC',
-        name: 'Seonhwa',
-        email: 'seonhwa@gmail.com',
-    },
-];
+import { getUsers } from '../db/database.js';
+import MongoDb from 'mongodb';
 
+const ObjectId = MongoDb.ObjectId;
 export async function findByUsername(username) {
-    return users.find((user) => user.username === username);
+    return getUsers() //
+        .findOne({ username }) //
+        .then(mapOptionalUser);
 }
 
 export async function findById(id) {
-    return users.find((user) => user.id === id);
+    return getUsers() //
+        .findOne({ _id: new ObjectId(id) }) //
+        .then(mapOptionalUser);
 }
 
 export async function createUser(user) {
-    const created = { ...user, id: Date.now().toString() };
-    users.push(created);
-    return created.id;
+    return getUsers() //
+        .insertOne(user) //
+        .then((data) => {
+            return data.insertedId.toString();
+        });
+}
+
+function mapOptionalUser(user) {
+    return user ? { ...user, id: user._id } : user;
 }
